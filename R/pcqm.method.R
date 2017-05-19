14b3c25b73bd72df5b4de91a4a8cb1f6cb5efe8c
Geoffrey S Shideler <geoffreyshideler@gmail.com>
2017-05-19 10:15:42 -0400

    Update pcqm.method.R

--- R/pcqm.method.R
+++ R/pcqm.method.R
@@ -160,7 +160,35 @@ pcqm.method <- function(x,
             relative$Relative_Dominance <- paste(relative$Relative_Dominance, "%")
             print.noquote(relative, row.names = FALSE)
            
-  relative
+  if(ivplot == TRUE){
+  # PLOT RELATIVE IMPORATNCE DATA
+  compare2 <- compare[order(compare$Species),]
+  compare2 <- subset(compare2, compare2$Rank<6)
+  MX <- data.frame(100, 100, 100)
+  names(MX) <- c("Rel_Dom", "Rel_Den", "Rel_Fre")
+  MN <- data.frame(0, 0, 0)
+  names(MN) <- c("Rel_Dom", "Rel_Den", "Rel_Fre")
+  Rel_Dom <- data.frame(compare2$Relative_Dominance)
+  names(Rel_Dom)<-"Rel_Dom"
+  Rel_Den <- data.frame(compare2$Relative_Density)
+  names(Rel_Den)<-"Rel_Den"
+  Rel_Fre <- data.frame(compare2$Relative_Frequency)
+  names(Rel_Fre) <- "Rel_Fre"
+  
+  plotdata <- cbind(Rel_Dom, Rel_Fre, Rel_Den)
+  plotdata2 <- rbind(MX, MN, plotdata)
+  
+  colors_border=c( "firebrick", "black" , "dodgerblue", "goldenrod1", "lightgreen" )
+  colors_in=c( "firebrick", "black" , "dodgerblue", "goldenrod1", "lightgreen" )
+  speclist <- compare2$Species
+  
+  # Plot radarChart
+  fmsb::radarchart(plotdata2, maxmin=TRUE,
+                   axistype=1 , centerzero = TRUE, vlabels=c("Relative\ndominance (%)", "Relative\ndensity (%)", "Relative\nfrequency (%)"),
+                   pcol=colors_border,  plwd=4 , plty=1, 
+                   cglcol="grey", cglty=1, axislabcol="black", seg=5, caxislabels=seq(0,100,20), cglwd=0.8)
+  legend(.65, .8, legend = speclist, col=colors_in, title = "Species", seg.len = 2,  pch = 16, lwd=4,lty = 1, bty="n",box.col="grey")
+}
 }
 
 
