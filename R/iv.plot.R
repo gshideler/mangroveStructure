f807d6524f8ea9a8023e1bd2b0b3804994cab886
Geoffrey S Shideler <geoffreyshideler@gmail.com>
2017-06-01 16:17:13 -0400

    Update iv.plot.R

--- R/iv.plot.R
+++ R/iv.plot.R
@@ -1,20 +1,20 @@
-iv.plot <- function(x, Species = 'Species',
+iv.plot <- function(x, colors = NULL,
+                    Species = 'Species',
                     Relative_Density = 'Relative_Density',
                     Relative_Dominance = 'Relative_Dominance',
                     Relative_Frequency = 'Relative_Frequency',
                     Importance_Value = 'Importance_Value',
                     Rank = 'Rank'){
-x <- as.data.frame(x)
+    x <- as.data.frame(x)
 
-x$Relative_Density <- x[,Relative_Density]
-x$Relative_Dominance <- x[,Relative_Dominance]
-x$Relative_Frequency <- x[,Relative_Frequency]
-x$Importance_Value <- x[,Importance_Value]
-x$Rank <- x[,Rank]
+    x$Relative_Density <- x[,Relative_Density]
+    x$Relative_Dominance <- x[,Relative_Dominance]
+    x$Relative_Frequency <- x[,Relative_Frequency]
+    x$Importance_Value <- x[,Importance_Value]
+    x$Rank <- x[,Rank]
 
   # PLOT RELATIVE IMPORATNCE DATA
   compare <- x[order(x$Species),]
-  compare2 <- subset(compare, compare$Rank<6)
   MX <- data.frame(100, 100, 100)
   names(MX) <- c("Rel_Dom", "Rel_Den", "Rel_Fre")
   MN <- data.frame(0, 0, 0)
@@ -29,15 +29,22 @@ x$Rank <- x[,Rank]
   plotdata <- cbind(Rel_Dom, Rel_Fre, Rel_Den)
   plotdata2 <- rbind(MX, MN, plotdata)
   
-  colors_border=c( "firebrick", "black" , "dodgerblue", "goldenrod1", "lightgreen" )
-  colors_in=c( "firebrick", "black" , "dodgerblue", "goldenrod1", "lightgreen" )
+  if(is.null(colors)){
+  colors <- c( "firebrick", "black" , "dodgerblue", "goldenrod1", "lightgreen" )
+  }
+  else{
+    colors <- colors
+    if(length(colors) != length(unique(compare$Species))) stop("If specifying colors, one color must be listed for each species (listed alphabetically)")
+
+  }
+  
   speclist <- compare$Species
   
   # Plot radarChart
   fmsb::radarchart(plotdata2, maxmin=TRUE,
                    axistype=1 , centerzero = TRUE, vlabels=c("Relative\ndominance (%)", "Relative\ndensity (%)", "Relative\nfrequency (%)"),
-                   pcol=colors_border,  plwd=4 , plty=1, 
+                   pcol=colors,  plwd=4 , plty=1, 
                    cglcol="grey", cglty=1, axislabcol="black", seg=5, caxislabels=seq(0,100,20), cglwd=0.8)
-  legend(.65, .8, legend = speclist, col=colors_in, title = "Species", seg.len = 2,  pch = 16, lwd=4,lty = 1, bty="n",box.col="grey")
+  legend(.65, .8, legend = speclist, col=colors, title = "Species", seg.len = 2,  pch = 16, lwd=4,lty = 1, bty="n",box.col="grey")
 }
 
