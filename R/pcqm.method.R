0a6d4b7c302658a63a0c40540559a01eb2ec0583
Geoffrey S Shideler <geoffreyshideler@gmail.com>
2017-07-21 13:04:03 -0400

    Update pcqm.method.R

--- R/pcqm.method.R
+++ R/pcqm.method.R
@@ -86,7 +86,7 @@ pcqm.method <- function(x,
 
 
             # Calculate and print density summaries
-            cat("\n\n DENSITY COMPUTATION\n -----\n")
+            cat("\n DENSITY COMPUTATION\n -----\n")
             x$static <- 1
             densum <- plyr::ddply(x, "static", transform, sum.n = max(count), meandist= mean(Distance), stemcalc=round((1/(mean(Distance)^2))*1000, digits=1))
             densum1 <- plyr::ddply(densum, "Species", summarize, Number = length(static), Proportion = round(length(static) / max(sum.n), digits=2), stemcalc=max(stemcalc))
@@ -97,7 +97,7 @@ pcqm.method <- function(x,
             cat(paste(" Total density per 0.1 ha =", sum(densum2$Stems_Per_0.1_Ha)))
 
             # Calculate and print basal area summaries
-            cat("\n\n BASAL AREA COMPUTATION\n -----\n")
+            cat("\n BASAL AREA COMPUTATION\n -----\n")
             densum$BA <- (3.14159265359 * (densum$dbh/2) ^ 2) / 10000
             densumBA <- plyr::ddply(densum, "Species", summarize, MeanBA = round(mean(BA), digits=4))
             densumBA <- plyr::join(densum2, densumBA, by="Species")
