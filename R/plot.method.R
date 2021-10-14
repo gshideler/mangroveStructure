055f46e48514180ff73dfd5c7254850e469a0e5e
Geoffrey S Shideler <geoffreyshideler@gmail.com>
2021-10-14 13:25:50 -0600

    Update plot.method.R

--- R/plot.method.R
+++ R/plot.method.R
@@ -1,7 +1,7 @@
 #' Mangrove Plot Method Analysis
 #'
 #' This function allows you to estimate mangrove forest structure based on sampling using fixed-area sampling (plot).
-#' @param plotnumber Column name in data frame for plot nymber (numerical). Default name is "plotnumber". First plot number must be 1.
+#' @param plotnumber Column name in data frame for plot number (numerical). Default name is "plotnumber". First plot number must be 1.
 #' @param dbh Column name in data frame for diameter at breast height. Default name is "dbh". Values must be in centimeters.
 #' @param species Column name in data frame for species. Default name is "species".
 #' @param height Column name in data frame for height (Optional). Default name is "height". Values must be in meters. When included, additional height-related outputs are displayed.
